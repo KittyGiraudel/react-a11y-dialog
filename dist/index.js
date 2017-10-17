7760a81e9beba358b17e75e1f9f5b1968e40232a
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-10-17 12:32:30 +0200

    Moved code to ES6+

--- dist/index.js
+++ dist/index.js
@@ -1,162 +1,184 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var A11yDialog = require('a11y-dialog')
-var createReactClass = require('create-react-class')
-var PropTypes = require('prop-types')
+'use strict';
 
-var Dialog = createReactClass({
-  propTypes: {
-    // The HTML `id` attribute of the dialog element, internally used by
-    // a11y-dialog to manipulate the dialog.
-    id: PropTypes.string.isRequired,
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    // The title of the dialog, mandatory in the document to provide context to
-    // assistive technology. Could be hidden (while remaining accessible) with
-    // CSS though.
-    title: PropTypes.string.isRequired,
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    // A function called when the component has mounted, receiving the instance
-    // of A11yDialog so that it can be programmatically accessed later on.
-    // E.g.: dialogRef={(dialog) => (this.dialog = dialog)}
-    dialogRef: PropTypes.func.isRequired,
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-    // The HTML `id` attribute of the dialog’s title element, used by assistive
-    // technologies to provide context and meaning to the dialog window. Falls
-    // back to the `${this.props.id}-title` if not provided.
-    titleId: PropTypes.string,
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    // The HTML `aria-label` attribute of the close button, used by assistive
-    // technologies to provide extra meaning to the usual cross-mark. Defaults
-    // to a generic English explanation.
-    closeButtonLabel: PropTypes.string,
+var React = require('react');
+var ReactDOM = require('react-dom');
+var A11yDialog = require('a11y-dialog');
+var PropTypes = require('prop-types');
 
-    // The string that is the innerHTML of the close button.
-    closeButtonContent: PropTypes.string,
+var Dialog = function (_React$Component) {
+  _inherits(Dialog, _React$Component);
 
-    // React 16 requires a container for the portal’s content to be rendered
-    // into; this is required and needs to be an existing valid DOM node,
-    // adjacent to the React root container of the application.
-    dialogContainer: PropTypes.string.isRequired,
+  function Dialog(props) {
+    _classCallCheck(this, Dialog);
 
-    // When rendering the component for the first time, the dialog has not been
-    // initialised yet and there is no way to figure whether the dialog should
-    // be open or closed on load. This sets the initial value for the
-    // `aria-hidden` attribute and defaults to `true` when omitted.
-    initiallyHidden: PropTypes.bool,
+    var _this = _possibleConstructorReturn(this, (Dialog.__proto__ || Object.getPrototypeOf(Dialog)).call(this, props));
 
-    // Object of classes for each HTML element of the dialog element. Keys are:
-    // - base
-    // - overlay
-    // - element
-    // - document
-    // - title
-    // - closeButton
-    // See for reference: http://edenspiekermann.github.io/a11y-dialog/#expected-dom-structure
-    classNames: PropTypes.objectOf(PropTypes.string)
-  },
-
-  getInitialState: function () {
-    return {
+    _this.state = {
       isMounted: false,
-      node: undefined
-    }
-  },
-
-  getDefaultProps: function () {
-    return {
-      closeButtonLabel: 'Close this dialog window',
-      closeButtonContent: '\u00D7',
-      initiallyHidden: true,
-      classNames: {}
-      // Default properties cannot be based on other properties, so the default
-      // value for the `titleId` prop is defined in the `render(..)` method.
-    }
-  },
-
-  componentDidMount: function () {
-    this.setState({ isMounted: true })
-  },
-
-  componentDidUpdate: function (prevProps, prevState) {
-    if (prevState.node !== this.state.node && this.state.node) {
-      this.dialog = this.dialog || this.initDialog()
-      this.props.dialogRef(this.dialog)
-    }
-  },
-
-  componentWillUnmount: function () {
-    this.dialog.destroy()
-    this.props.dialogRef(undefined)
-  },
-
-  initDialog: function () {
-    return new A11yDialog(
-      this.state.node,
-      document.querySelector(this.props.rootSelector)
-    )
-  },
-
-  close: function () {
-    this.dialog.hide()
-  },
-
-  handleRef: function (node) {
-    this.setState({ node: node })
-  },
-
-  render: function () {
-    if (!this.state.isMounted) {
-      return null
-    }
-
-    const id = this.props.id
-    const classNames = this.props.classNames
-    const titleId = this.props.titleId || (id + '-title')
-
-    return ReactDOM.createPortal(
-      React.createElement("div", {
-        id: id, 
-        className: classNames.base, 
-        ref: this.handleRef}, 
-
-        React.createElement("div", {
-          tabIndex: "-1", 
-          className: classNames.overlay, 
-          onClick: this.close}), 
-
-        React.createElement("div", {
-          role: "dialog", 
-          className: classNames.element, 
-          "aria-labelledby": titleId}, 
-
-          React.createElement("div", {
-            role: "document", 
-            className: classNames.document}, 
-
-            React.createElement("button", {
-              type: "button", 
-              "aria-label": this.props.closeButtonLabel, 
-              onClick: this.close, 
-              className: classNames.closeButton}, 
-              this.props.closeButtonContent
-            ), 
-
-            React.createElement("h1", {
-              id: titleId, 
-              className: classNames.title}, 
-              this.props.title
-            ), 
-
-            this.props.children
-
-          )
-
-        )
-
-      ),
-      document.querySelector(this.props.dialogContainer)
-    )
+      node: null
+    };
+    return _this;
   }
-})
 
-module.exports = Dialog
+  _createClass(Dialog, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.setState({ isMounted: true });
+    }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate(prevProps, prevState) {
+      if (prevState.node !== this.state.node && this.state.node) {
+        this.dialog = this.dialog || this.initDialog();
+        this.props.dialogRef(this.dialog);
+      }
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      this.dialog.destroy();
+      this.props.dialogRef(undefined);
+    }
+  }, {
+    key: 'initDialog',
+    value: function initDialog() {
+      return new A11yDialog(this.state.node, document.querySelector(this.props.rootSelector));
+    }
+  }, {
+    key: 'close',
+    value: function close() {
+      this.dialog.hide();
+    }
+  }, {
+    key: 'handleRef',
+    value: function handleRef(node) {
+      this.setState({ node: node });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      if (!this.state.isMounted) {
+        return null;
+      }
+
+      var _props = this.props,
+          id = _props.id,
+          classNames = _props.classNames;
+
+      var titleId = this.props.titleId || id + '-title';
+
+      return ReactDOM.createPortal(React.createElement(
+        'div',
+        {
+          id: id,
+          className: classNames.base,
+          ref: this.handleRef },
+        React.createElement('div', {
+          tabIndex: '-1',
+          className: classNames.overlay,
+          onClick: this.close }),
+        React.createElement(
+          'div',
+          {
+            role: 'dialog',
+            className: classNames.element,
+            'aria-labelledby': titleId },
+          React.createElement(
+            'div',
+            {
+              role: 'document',
+              className: classNames.document },
+            React.createElement(
+              'button',
+              {
+                type: 'button',
+                'aria-label': this.props.closeButtonLabel,
+                onClick: this.close,
+                className: classNames.closeButton },
+              this.props.closeButtonContent
+            ),
+            React.createElement(
+              'h1',
+              {
+                id: titleId,
+                className: classNames.title },
+              this.props.title
+            ),
+            this.props.children
+          )
+        )
+      ), document.querySelector(this.props.dialogContainer));
+    }
+  }]);
+
+  return Dialog;
+}(React.Component);
+
+Dialog.defaultProps = {
+  closeButtonLabel: 'Close this dialog window',
+  closeButtonContent: '\xD7',
+  initiallyHidden: true,
+  classNames: {}
+  // Default properties cannot be based on other properties, so the default
+  // value for the `titleId` prop is defined in the `render(..)` method.
+};
+
+Dialog.propTypes = {
+  // The HTML `id` attribute of the dialog element, internally used by
+  // a11y-dialog to manipulate the dialog.
+  id: PropTypes.string.isRequired,
+
+  // The title of the dialog, mandatory in the document to provide context to
+  // assistive technology. Could be hidden (while remaining accessible) with
+  // CSS though.
+  title: PropTypes.string.isRequired,
+
+  // A function called when the component has mounted, receiving the instance
+  // of A11yDialog so that it can be programmatically accessed later on.
+  // E.g.: dialogRef={(dialog) => (this.dialog = dialog)}
+  dialogRef: PropTypes.func.isRequired,
+
+  // The HTML `id` attribute of the dialog’s title element, used by assistive
+  // technologies to provide context and meaning to the dialog window. Falls
+  // back to the `${this.props.id}-title` if not provided.
+  titleId: PropTypes.string,
+
+  // The HTML `aria-label` attribute of the close button, used by assistive
+  // technologies to provide extra meaning to the usual cross-mark. Defaults
+  // to a generic English explanation.
+  closeButtonLabel: PropTypes.string,
+
+  // The string that is the innerHTML of the close button.
+  closeButtonContent: PropTypes.string,
+
+  // React 16 requires a container for the portal’s content to be rendered
+  // into; this is required and needs to be an existing valid DOM node,
+  // adjacent to the React root container of the application.
+  dialogContainer: PropTypes.string.isRequired,
+
+  // When rendering the component for the first time, the dialog has not been
+  // initialised yet and there is no way to figure whether the dialog should
+  // be open or closed on load. This sets the initial value for the
+  // `aria-hidden` attribute and defaults to `true` when omitted.
+  initiallyHidden: PropTypes.bool,
+
+  // Object of classes for each HTML element of the dialog element. Keys are:
+  // - base
+  // - overlay
+  // - element
+  // - document
+  // - title
+  // - closeButton
+  // See for reference: http://edenspiekermann.github.io/a11y-dialog/#expected-dom-structure
+  classNames: PropTypes.objectOf(PropTypes.string)
+};
+
+module.exports = Dialog;
--- index.js
+++ index.js
@@ -1,116 +1,55 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var A11yDialog = require('a11y-dialog')
-var createReactClass = require('create-react-class')
-var PropTypes = require('prop-types')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const A11yDialog = require('a11y-dialog')
+const PropTypes = require('prop-types')
 
-var Dialog = createReactClass({
-  propTypes: {
-    // The HTML `id` attribute of the dialog element, internally used by
-    // a11y-dialog to manipulate the dialog.
-    id: PropTypes.string.isRequired,
+class Dialog extends React.Component {
+  constructor (props) {
+    super(props)
 
-    // The title of the dialog, mandatory in the document to provide context to
-    // assistive technology. Could be hidden (while remaining accessible) with
-    // CSS though.
-    title: PropTypes.string.isRequired,
-
-    // A function called when the component has mounted, receiving the instance
-    // of A11yDialog so that it can be programmatically accessed later on.
-    // E.g.: dialogRef={(dialog) => (this.dialog = dialog)}
-    dialogRef: PropTypes.func.isRequired,
-
-    // The HTML `id` attribute of the dialog’s title element, used by assistive
-    // technologies to provide context and meaning to the dialog window. Falls
-    // back to the `${this.props.id}-title` if not provided.
-    titleId: PropTypes.string,
-
-    // The HTML `aria-label` attribute of the close button, used by assistive
-    // technologies to provide extra meaning to the usual cross-mark. Defaults
-    // to a generic English explanation.
-    closeButtonLabel: PropTypes.string,
-
-    // The string that is the innerHTML of the close button.
-    closeButtonContent: PropTypes.string,
-
-    // React 16 requires a container for the portal’s content to be rendered
-    // into; this is required and needs to be an existing valid DOM node,
-    // adjacent to the React root container of the application.
-    dialogContainer: PropTypes.string.isRequired,
-
-    // When rendering the component for the first time, the dialog has not been
-    // initialised yet and there is no way to figure whether the dialog should
-    // be open or closed on load. This sets the initial value for the
-    // `aria-hidden` attribute and defaults to `true` when omitted.
-    initiallyHidden: PropTypes.bool,
-
-    // Object of classes for each HTML element of the dialog element. Keys are:
-    // - base
-    // - overlay
-    // - element
-    // - document
-    // - title
-    // - closeButton
-    // See for reference: http://edenspiekermann.github.io/a11y-dialog/#expected-dom-structure
-    classNames: PropTypes.objectOf(PropTypes.string)
-  },
-
-  getInitialState: function () {
-    return {
+    this.state = {
       isMounted: false,
-      node: undefined
+      node: null
     }
-  },
+  }
 
-  getDefaultProps: function () {
-    return {
-      closeButtonLabel: 'Close this dialog window',
-      closeButtonContent: '\u00D7',
-      initiallyHidden: true,
-      classNames: {}
-      // Default properties cannot be based on other properties, so the default
-      // value for the `titleId` prop is defined in the `render(..)` method.
-    }
-  },
-
-  componentDidMount: function () {
+  componentDidMount () {
     this.setState({ isMounted: true })
-  },
+  }
 
-  componentDidUpdate: function (prevProps, prevState) {
+  componentDidUpdate (prevProps, prevState) {
     if (prevState.node !== this.state.node && this.state.node) {
       this.dialog = this.dialog || this.initDialog()
       this.props.dialogRef(this.dialog)
     }
-  },
+  }
 
-  componentWillUnmount: function () {
+  componentWillUnmount () {
     this.dialog.destroy()
     this.props.dialogRef(undefined)
-  },
+  }
 
-  initDialog: function () {
+  initDialog () {
     return new A11yDialog(
       this.state.node,
       document.querySelector(this.props.rootSelector)
     )
-  },
+  }
 
-  close: function () {
+  close () {
     this.dialog.hide()
-  },
+  }
 
-  handleRef: function (node) {
+  handleRef (node) {
     this.setState({ node: node })
-  },
+  }
 
-  render: function () {
+  render () {
     if (!this.state.isMounted) {
       return null
     }
 
-    const id = this.props.id
-    const classNames = this.props.classNames
+    const { id, classNames } = this.props
     const titleId = this.props.titleId || (id + '-title')
 
     return ReactDOM.createPortal(
@@ -157,6 +96,65 @@ var Dialog = createReactClass({
       document.querySelector(this.props.dialogContainer)
     )
   }
-})
+}
+
+Dialog.defaultProps = {
+  closeButtonLabel: 'Close this dialog window',
+  closeButtonContent: '\u00D7',
+  initiallyHidden: true,
+  classNames: {}
+  // Default properties cannot be based on other properties, so the default
+  // value for the `titleId` prop is defined in the `render(..)` method.
+}
+
+Dialog.propTypes = {
+  // The HTML `id` attribute of the dialog element, internally used by
+  // a11y-dialog to manipulate the dialog.
+  id: PropTypes.string.isRequired,
+
+  // The title of the dialog, mandatory in the document to provide context to
+  // assistive technology. Could be hidden (while remaining accessible) with
+  // CSS though.
+  title: PropTypes.string.isRequired,
+
+  // A function called when the component has mounted, receiving the instance
+  // of A11yDialog so that it can be programmatically accessed later on.
+  // E.g.: dialogRef={(dialog) => (this.dialog = dialog)}
+  dialogRef: PropTypes.func.isRequired,
+
+  // The HTML `id` attribute of the dialog’s title element, used by assistive
+  // technologies to provide context and meaning to the dialog window. Falls
+  // back to the `${this.props.id}-title` if not provided.
+  titleId: PropTypes.string,
+
+  // The HTML `aria-label` attribute of the close button, used by assistive
+  // technologies to provide extra meaning to the usual cross-mark. Defaults
+  // to a generic English explanation.
+  closeButtonLabel: PropTypes.string,
+
+  // The string that is the innerHTML of the close button.
+  closeButtonContent: PropTypes.string,
+
+  // React 16 requires a container for the portal’s content to be rendered
+  // into; this is required and needs to be an existing valid DOM node,
+  // adjacent to the React root container of the application.
+  dialogContainer: PropTypes.string.isRequired,
+
+  // When rendering the component for the first time, the dialog has not been
+  // initialised yet and there is no way to figure whether the dialog should
+  // be open or closed on load. This sets the initial value for the
+  // `aria-hidden` attribute and defaults to `true` when omitted.
+  initiallyHidden: PropTypes.bool,
+
+  // Object of classes for each HTML element of the dialog element. Keys are:
+  // - base
+  // - overlay
+  // - element
+  // - document
+  // - title
+  // - closeButton
+  // See for reference: http://edenspiekermann.github.io/a11y-dialog/#expected-dom-structure
+  classNames: PropTypes.objectOf(PropTypes.string)
+}
 
 module.exports = Dialog
