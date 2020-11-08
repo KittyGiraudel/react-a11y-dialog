7623ec7df55d45b99c203bc8a7235745f2a5d7a0
Moritz Kröger <write@morkro.de>
2020-11-08 16:34:32 +0100

    Revert import syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import A11yDialog from 'a11y-dialog'
-import PropTypes from 'prop-types'
+const React = require('react')
+const ReactDOM = require('react-dom')
+const A11yDialog = require('a11y-dialog')
+const PropTypes = require('prop-types')
 
 const useIsMounted = () => {
   const [isMounted, setIsMounted] = React.useState(false)
