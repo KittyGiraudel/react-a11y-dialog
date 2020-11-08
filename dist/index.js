9d35a9eba38d0e530cd8b9058220097ef41af9de
Hugo Giraudel <hugo.giraudel@gmail.com>
2020-11-08 11:30:40 +0100

    Reauthor component as a function component

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,16 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var React = require('react');
 
@@ -30,111 +20,70 @@ var A11yDialog = require('a11y-dialog');
 
 var PropTypes = require('prop-types');
 
-var Dialog = /*#__PURE__*/function (_React$Component) {
-  _inherits(Dialog, _React$Component);
+var useIsMounted = function useIsMounted() {
+  var _React$useState = React.useState(false),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      isMounted = _React$useState2[0],
+      setIsMounted = _React$useState2[1];
 
-  var _super = _createSuper(Dialog);
+  React.useEffect(function () {
+    return setIsMounted(true);
+  }, []);
+  return isMounted;
+};
 
-  function Dialog(props) {
-    var _this;
+var Dialog = function Dialog(props) {
+  var isMounted = useIsMounted();
+  var container = React.useRef(null);
+  var instance = React.useRef(null);
+  var dialogRef = props.dialogRef,
+      appRoot = props.appRoot;
+  React.useEffect(function () {
+    if (container.current) {
+      instance.current = new A11yDialog(container.current, appRoot);
+      dialogRef(instance.current);
+    }
 
-    _classCallCheck(this, Dialog);
-
-    _this = _super.call(this, props);
-    _this.state = {
-      isMounted: false,
-      container: null
+    return function () {
+      if (instance.current) instance.current.destroy();
+      dialogRef(undefined);
     };
-    _this.initDialog = _this.initDialog.bind(_assertThisInitialized(_this));
-    _this.close = _this.close.bind(_assertThisInitialized(_this));
-    _this.handleRef = _this.handleRef.bind(_assertThisInitialized(_this));
-    return _this;
-  }
-
-  _createClass(Dialog, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      this.setState({
-        isMounted: true
-      });
-    }
-  }, {
-    key: "componentDidUpdate",
-    value: function componentDidUpdate(prevProps, prevState) {
-      if (prevState.container !== this.state.container && this.state.container) {
-        this.dialog = this.dialog || this.initDialog();
-        this.props.dialogRef(this.dialog);
-      }
-    }
-  }, {
-    key: "componentWillUnmount",
-    value: function componentWillUnmount() {
-      if (this.dialog) {
-        this.dialog.destroy();
-      }
-
-      this.props.dialogRef(undefined);
-    }
-  }, {
-    key: "initDialog",
-    value: function initDialog() {
-      return new A11yDialog(this.state.container, this.props.appRoot);
-    }
-  }, {
-    key: "close",
-    value: function close() {
-      this.dialog.hide();
-    }
-  }, {
-    key: "handleRef",
-    value: function handleRef(element) {
-      this.setState({
-        container: element
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      if (!this.state.isMounted) {
-        return null;
-      }
-
-      var _this$props = this.props,
-          id = _this$props.id,
-          classNames = _this$props.classNames;
-      var titleId = this.props.titleId || id + '-title';
-      var Element = this.props.useDialog ? 'dialog' : 'div';
-      return ReactDOM.createPortal( /*#__PURE__*/React.createElement("div", {
-        id: id,
-        className: classNames.base,
-        ref: this.handleRef
-      }, /*#__PURE__*/React.createElement("div", {
-        tabIndex: "-1",
-        className: classNames.overlay,
-        onClick: this.props.role === 'alertdialog' ? undefined : this.close
-      }), /*#__PURE__*/React.createElement(Element, {
-        role: this.props.role,
-        className: classNames.element,
-        "aria-labelledby": titleId
-      }, /*#__PURE__*/React.createElement("div", {
-        role: this.props.useDialog ? undefined : 'document',
-        className: classNames.document
-      }, /*#__PURE__*/React.createElement("button", {
-        type: "button",
-        "aria-label": this.props.closeButtonLabel,
-        onClick: this.close,
-        className: classNames.closeButton
-      }, this.props.closeButtonContent), /*#__PURE__*/React.createElement("p", {
-        id: titleId,
-        className: classNames.title,
-        role: "heading",
-        "aria-level": "1"
-      }, this.props.title), this.props.children))), document.querySelector(this.props.dialogRoot));
-    }
-  }]);
-
-  return Dialog;
-}(React.Component);
+  }, [container, dialogRef, appRoot]);
+  var close = React.useCallback(function () {
+    return instance.current.hide();
+  }, []);
+  if (!isMounted) return null;
+  var id = props.id,
+      classNames = props.classNames;
+  var titleId = props.titleId || id + '-title';
+  var Element = props.useDialog ? 'dialog' : 'div';
+  return ReactDOM.createPortal( /*#__PURE__*/React.createElement("div", {
+    id: id,
+    className: classNames.base,
+    ref: container
+  }, /*#__PURE__*/React.createElement("div", {
+    tabIndex: "-1",
+    className: classNames.overlay,
+    onClick: props.role === 'alertdialog' ? undefined : close
+  }), /*#__PURE__*/React.createElement(Element, {
+    role: props.role,
+    className: classNames.element,
+    "aria-labelledby": titleId
+  }, /*#__PURE__*/React.createElement("div", {
+    role: props.useDialog ? undefined : 'document',
+    className: classNames.document
+  }, /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    "aria-label": props.closeButtonLabel,
+    onClick: close,
+    className: classNames.closeButton
+  }, props.closeButtonContent), /*#__PURE__*/React.createElement("p", {
+    id: titleId,
+    className: classNames.title,
+    role: "heading",
+    "aria-level": "1"
+  }, props.title), props.children))), document.querySelector(props.dialogRoot));
+};
 
 Dialog.defaultProps = {
   role: 'dialog',
